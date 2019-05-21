363e58b3fda496e4274a2fd113efec0578d8ed41
mgmk2 <xis.t65@gmail.com>
2019-05-21 22:22:13 +0900

    abc099 c (not passed)

--- ABC/099/C.py
+++ ABC/099/C.py
@@ -1,10 +1,15 @@
-import math
+def log(x, y):
+    i = 0
+    while(y ** i <= x):
+        i += 1
+    return i - 1
+
 N = int(input())
 if N < 6:
     ans = N
 else:
-    a = int(math.log(N, 6))
-    b = int(math.log(N, 9))
+    a = log(N, 6)
+    b = log(N, 9)
     print(a)
     print(b)
     m = [6**i for i in range(1, a + 1)] + [9**j for j in range(1, b + 1)]
@@ -13,10 +18,9 @@ else:
     i = 0
     ans = 0
     while(True):
-        if nn >= m[i]:
-            print(nn, m[i])
-            ans += nn // m[i]
-            nn = nn % m[i]
+        print(nn, m[i], nn // m[i])
+        ans += nn // m[i]
+        nn = nn % m[i]
         i += 1
         if i == len(m):
             ans += nn
