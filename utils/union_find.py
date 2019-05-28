5c546102a47bfca9a26d3b458a67b28fc64977d7
mgmk2 <xis.t65@gmail.com>
2019-05-29 00:57:07 +0900

    add check and size method to UnionFind class

--- utils/union_find.py
+++ utils/union_find.py
@@ -2,6 +2,7 @@ class UnionFind(object):
     def __init__(self, n):
         self.par = [i for i in range(n + 1)]
         self.rank = [0 for i in range(n + 1)]
+        self.member = [1 for i in range(n + 1)]
 
     def find(self, x):
         if self.par[x] == x:
@@ -9,13 +10,22 @@ class UnionFind(object):
         self.par[x] = self.find(self.par[x])
         return self.par[x]
 
+    def check(self, x, y):
+        return self.find(x) == self.find(y)
+
     def union(self, x, y):
         x = self.find(x)
         y = self.find(y)
         if self.rank[x] < self.rank[y]:
             self.par[x] = y
+            self.member[y] += self.member[x]
         elif self.rank[x] > self.rank[y]:
             self.par[y] = x
+            self.member[x] += self.member[y]
         else:
             self.par[y] = x
             self.rank[x] += 1
+            self.member[x] += self.member[y]
+
+    def size(self, x):
+        return self.member[self.par[x]]
