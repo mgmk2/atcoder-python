17d0205992300e870e292348085e5a1ff9cdac39
mgmk2 <xis.t65@gmail.com>
2019-06-17 15:24:50 +0900

    abc130 e

--- ABC/130/e.py
+++ ABC/130/e.py
@@ -2,10 +2,12 @@ n, m = map(int, input().split())
 s = list(map(int, input().split()))
 t = list(map(int, input().split()))
 MOD = 10 ** 9 + 7
-dp = [[[0, 0] for j in range(m + 1)] for i in range(n + 1)]
+dp = [[0 for j in range(m + 1)] for i in range(2)]
 for i, si in enumerate(s):
     for j, tj in enumerate(t):
+        idx = (i + 1) % 2
+        dp[idx][j + 1] = (dp[idx][j] + dp[1 - idx][j + 1] - dp[1 - idx][j]) % MOD
         if si == tj:
-            dp[i + 1][j + 1][0] = (dp[i][j][1] + 1) % MOD
-        dp[i + 1][j + 1][1] = (dp[i + 1][j][1] + dp[i][j + 1][1] - dp[i][j][1] + dp[i + 1][j + 1][0]) % MOD
-print(dp)
+            dp[idx][j + 1] += dp[1 - idx][j] + 1
+        dp[idx][j + 1] %= MOD
+print((dp[n % 2][-1] + 1) % MOD)
