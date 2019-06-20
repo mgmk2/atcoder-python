48c7959a3bf4e896ac5a716a7274593ed3a3a8fe
mgmk2 <xis.t65@gmail.com>
2019-06-20 15:44:21 +0900

    abc120 d

--- ABC/120/d.py
+++ ABC/120/d.py
@@ -28,7 +28,7 @@ class UnionFind(object):
             self.member[x] += self.member[y]
 
     def size(self, x):
-        return self.member[self.par[x]]
+        return self.member[self.find(x)]
 
 n, m = map(int, input().split())
 E = [list(map(int, input().split())) for _ in range(m)]
@@ -41,5 +41,5 @@ for i, e in enumerate(E[::-1]):
     else:
         ans[-(i + 2)] = ans[-(i + 1)] -  tree.size(e[0]) * tree.size(e[1])
         tree.union(*e)
-for a in ans[1:]:
-    print(a)
+for p in tree.par[1:]:
+    print(p)
