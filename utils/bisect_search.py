09571869f2525d4a4079e2d51fbcfaa5f7cf5b84
mgmk2 <mgmk2.dev@gmail.com>
2021-05-04 16:00:18 +0900

    [update] update some utils functions and classes

--- utils/bisect_search.py
+++ utils/bisect_search.py
@@ -16,3 +16,39 @@ def bisect_search(L, x):
         else:
             start = center
     return y
+
+def lower_bound(x, f, start, end):
+    # bisect search x in [f(start), f(end - 1)]
+    # f: function
+
+    end -= 1
+    if x <= f(start):
+        return start
+    if x > f(end):
+        return end + 1
+
+    while start + 1 < end:
+        center = start + (end - start) // 2
+        if x <= f(center):
+            end = center
+        else:
+            start = center
+    return end
+
+def upper_bound(x, f, start, end):
+    # bisect search x in [f(start), f(end - 1)]
+    # f: function
+
+    end -= 1
+    if x < f(start):
+        return start
+    if x >= f(end):
+        return end + 1
+
+    while start + 1 < end:
+        center = start + (end - start) // 2
+        if x < f(center):
+            end = center
+        else:
+            start = center
+    return end
